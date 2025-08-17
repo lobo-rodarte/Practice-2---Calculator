e6a5d276b4561372558cbe35228d5e3d053d12b0
mrodarte <mlrfortune@gmail.com>
2025-08-17 13:06:40 -0500

    updated code, workiing through the calculator as the functions work appropriately.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,5 @@
+const readline = require('readline-sync');
+
 /* 
 1. Write a function for each operation listed above using the Math module. 
 2. Test each function with sample inputs to ensure it works as intended. 
@@ -10,38 +12,91 @@
 ● Generate a random number between 1 and 50. 
 ● Round 23.67891 to 2 decimal places. 
 */
+let calculations = {
+    absoluteValue: function(num1) {
+        return Math.abs(num1);
+    },
 
-function absoluteValue(num1) {
-    return Math.abs(num1);
-}
- 
-function powerCalc(num1, num2) {
-    return Math.pow(num1, num2);
-}
- 
-function squareRoot(num1) {
-    return Math.sqrt(num1);
-}
+    powerCalc: function(num1, num2) {
+        return Math.pow(num1, num2);
+    },
+    
+    squareRoot: function(num1) {
+        return Math.sqrt(num1);
+    },
 
-function maxFind(num1, num2, num3) {
-    return Math.max(num1, num2, num3);
-}
+    maxFind: function(num1, num2, num3) {
+        return Math.max(num1, num2, num3);
+    },
 
-function minFind(num1, num2, num3) {
-    return Math.min(num1, num2, num3);
-}
+    minFind: function(num1, num2, num3) {
+        return Math.min(num1, num2, num3);
+    },
 
-function randomNum(min, max) {
-    return Math.random() * (max - min) + min;
-}
-function customRound(num1) {
-    return Math.round(num1 * 100)/100
-}
+    randomNum: function(min, max) {
+        return Math.random() * (max - min) + min;
+    },
 
-console.log(absoluteValue(-45.67));
-console.log(powerCalc(5, 3));
-console.log(squareRoot(144));
-console.log(maxFind(3, 78, -12, 0.5, 27));
-console.log(minFind(3, 78, -12, 0.5, 27));
-console.log(randomNum(1, 50));
-console.log(customRound(23.67891))
+    customRound: function(num1) {
+        return Math.round(num1 * 100)/100
+    }
+};
+for (let key in calculations) {
+    if (key === 'absoluteValue') {
+}}
+// powerCalc, squareRoot, maxFind, minFind, randomNum, customRound */
+
+/*
+console.log(calculations.absoluteValue(-45.67));
+console.log(calculations.powerCalc(5, 3));
+console.log(calculations.squareRoot(144));
+console.log(calculations.maxFind(3, 78, -12, 0.5, 27));
+console.log(calculations.minFind(3, 78, -12, 0.5, 27));
+console.log(calculations.randomNum(1, 50));
+console.log(calculations.customRound(23.67891))
+*/
+
+let num1 = 1
+let num2 = 2
+let num3 = 3
+let min = 4
+let max = 5
+let singleNumberCalculationChoice = ['absoluteValue', 'squareRoot', 'customRound'];
+let doubleNumberCalculationChoice = ['powerCalc', 'maxFind', 'minFind', 'randomNum'];
+
+const welcomeMessage = readline.question(`Welcome student, are you ready to start your caluclations, click enter to start!\n`); 
+
+let calculationChoicePrompt = readline.question(`Please select your math method, we have: absoluteValue\npowerCalc,\nsquareRoot,\nmaxFind,\nminFind,\nrandomNum,\ncustomRound\nPlease select one.\n`); /* Used the multi-line strings from the STRINGING CHARACTERS AND STRINGS section to present the user with two options for a journey path */
+    while (!calculations[calculationChoicePrompt]) {
+        console.log('This is not a valid choice, please select a valid choice\n');
+        calculationChoicePrompt = readline.question(`Please select your math method.\n`)
+    } 
+    if(!doubleNumberCalculationChoice[calculationChoicePrompt]) {
+        console.log('There is only one number needed for your calculation')
+    }    
+
+let firstNumberInput = readline.question('Enter your first number\n')
+    while(isNaN(firstNumberInput)) {
+        firstNumberInput = readline.question('This is not a number, please input the correct data Thank you\n')
+    }
+
+let secondNumberInput = readline.question('Enter your second number\n')
+    while(isNaN(secondNumberInput)) {
+        secondNumberInput = readline.question('This is not a number, please input the correct data\n')
+    }
+
+// let finalCalculation = 
+
+// let initialNumber = num1
+/*
+let gatePassQuestion = readline.question('To pass through the gate, you must answer this question: What is the product of 3 and 4?\n'); {
+    while (gatePassQuestion !== numberThree.toString() && mathTry < maxTries) { // Used a while loop to validate the input
+        console.log('Incorrect. Please try again.');
+        gatePassQuestion = readline.question(`What is the product of ${numberOne} and ${numberTwo}?\n`); // Asked the user to answer the question again
+        mathTry++; // Incremented the attempt counter
+    };
+    if (gatePassQuestion === numberThree.toString()) { //converting the input to a string to accept the user input
+        console.log('Correct! You may pass through the gate.\n\nYou look up and read a sign that says: \n\"To slay the dragon, you must go left, bring your COURAGE as it will be the KEY to your success!\n\"To rescue the princess, you must go right, bring your wits and make haste as the KEY to freeing her will only come at high NOON!\"'); // Used a template literal to create a message
+    }
+}
+*/
