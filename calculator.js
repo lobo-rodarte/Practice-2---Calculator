7d60dd42c825d02261c37fa3a39f81ce95760a32
mrodarte <mlrfortune@gmail.com>
2025-08-17 19:54:57 -0500

    Finished the calculator and included notes as well for explanation

--- calculator.js
+++ calculator.js
@@ -12,42 +12,37 @@ const readline = require('readline-sync');
 ● Generate a random number between 1 and 50. 
 ● Round 23.67891 to 2 decimal places. 
 */
-let calculations = {
-    absoluteValue: function(num1) {
+
+let calculations = { // Put functions in this object to allow a check on method selection. If it does not match the list it doesn't allow further execution
+    absoluteValue: function(num1) { // Function for the absolute method
         return Math.abs(num1);
     },
 
-    powerCalc: function(num1, num2) {
+    powerCalc: function(num1, num2) { // Function for the power calculation method
         return Math.pow(num1, num2);
     },
     
-    squareRoot: function(num1) {
+    squareRoot: function(num1) { // Function for the square root method
         return Math.sqrt(num1);
     },
 
-    maxFind: function(num1, num2, num3) {
+    maxFind: function(num1, num2, num3) { // Function to find the highest number, only chose 3 numbers, in the output it brings them all together but kept it to 3 for the calculations
         return Math.max(num1, num2, num3);
     },
 
-    minFind: function(num1, num2, num3) {
+    minFind: function(num1, num2, num3) { // Function to find the lowest number, the reasoning for this is the same as above
         return Math.min(num1, num2, num3);
     },
 
-    randomNum: function(min, max) {
+    randomNum: function(min, max) { // Function to provide a random number between two numbers. The random() obtains a random number between 0 and 1 then multiplies the result of subtracting the min from the max then adds the min value to ensure the result is witin the min and max range.
         return Math.random() * (max - min) + min;
     },
 
-    customRound: function(num1) {
+    customRound: function(num1) { // Function to round to the nearest hunderedth. This moves the decimal over 2 decimal places then moves it back and cuts it off to only output the two decimal places.
         return Math.round(num1 * 100)/100
     }
 };
 
-/*for (let key in calculations) {
-    if (key === 'absoluteValue') {
-}}
-// powerCalc, squareRoot, maxFind, minFind, randomNum, customRound */
-
-/*
 console.log(calculations.absoluteValue(-45.67));
 console.log(calculations.powerCalc(5, 3));
 console.log(calculations.squareRoot(144));
@@ -55,10 +50,15 @@ console.log(calculations.maxFind(3, 78, -12, 0.5, 27));
 console.log(calculations.minFind(3, 78, -12, 0.5, 27));
 console.log(calculations.randomNum(1, 50));
 console.log(calculations.customRound(23.67891))
+
+/*
+Thought that this would be better to explain the code.
+I declared the num1 - num3 and result as undefined so that I can work them into the if, else if statements as I wanted to make this kind of dynamic. This way I can assign them later.
+I declared method with a condition and while statement to ensure that what was typed in would be in the list of methods built into the program, if it was there, then the user is prompted to input a valid choice.
+Once a valid choice is made, and depending on if the method requires 1, 2, or 3 numbers will depend on what they are prompted (added lines for the seperate ones), this is why the functions are in an object, to allow for the validation.
+Using the if, else, if statements allowed a multi-check on the selection of the user to pull the appropriate calculation.
 */
-let singleNumberCalculationChoice = ['absoluteValue', 'squareRoot', 'customRound'];
-let doubleNumberCalculationChoice = ['powerCalc', 'randomNum'];
-let moreThanTwoNumbersChoice = ['maxFind', 'minFind']
+
 let num1;
 let num2;
 let num3;
@@ -70,50 +70,38 @@ let method = readline.question('Please select your math method, we have:\nabsolu
         method = readline.question(`Please select your math method.\n`)
     }
     
-    if (method === 'absoluteValue') {
+    if (method === 'absoluteValue') { // single number method, so only one num1 is 
         num1 = parseFloat(readline.question('Enter your number\n'));
         result = calculations[method](num1);
         console.log(`Result: ${result}`);
+    } else if (method === 'squareRoot') {
+        num1 = parseFloat(readline.question('Enter your number\n'));
+        result = calculations[method](num1);
+        console.log(`Result: ${result}`);
+    } else if (method === 'customRound') {
+        num1 = parseFloat(readline.question('Enter your number\n'));
+        result = calculations[method](num1);
+        console.log(`Result: ${result}`);
+    } else if (method === 'minFind') { // this one has up to 3 numbers for the method to work, so num1, num2, and num3 are assigned
+        num1 = parseFloat(readline.question('Enter your number\n'));
+        num2 = parseFloat(readline.question('Enter your second number\n'));
+        num3 = parseFloat(readline.question('Enter your other number\n'));
+        result = calculations[method](num1, num2, num3);
+        console.log(`Result: ${result}`);
+    } else if (method === 'maxFind') {
+        num1 = parseFloat(readline.question('Enter your number\n'));
+        num2 = parseFloat(readline.question('Enter your second number\n'));
+        num3 = parseFloat(readline.question('Enter your other number\n'));
+        result = calculations[method](num1, num2, num3);
+        console.log(`Result: ${result}`);
+    } else if (method === 'powerCalc') { // this one has two numbers for the method to work, so num1 and num2 are assigned
+        num1 = parseFloat(readline.question('Enter your number\n'));
+        num2 = parseFloat(readline.question('Enter your second number\n'));
+        result = calculations[method](num1, num2);
+        console.log(`Result: ${result}`);
+    } else if (method === 'randomNum') {
+        num1 = parseFloat(readline.question('Enter your number\n'));
+        num2 = parseFloat(readline.question('Enter your second number\n'));
+        result = calculations[method](num1, num2);
+        console.log(`Result: ${result}`);
     }
-
-
-/*let calculationChoicePrompt = readline.question(`Please select your math method, we have:\nabsoluteValue\npowerCalc,\nsquareRoot,\nmaxFind,\nminFind,\nrandomNum,\ncustomRound\nPlease select one.\n`); */ // Used the multi-line strings from the STRINGING CHARACTERS AND STRINGS section to present the user with two options for a journey path 
-
-/*
-    if (doubleNumberCalculationChoice.includes(method)) {
-    num2 = parseFloat(readline.question('Enter your seond number'));
-    }
-    if (moreThanTwoNumbersChoice.includes(method)) {
-    num3 = parseFloat(readline.question('Enter your other number'));
-    }
-    if (calculationChoicePrompt === 'absoluteValue') {
-        console.log('There is only one number needed for your calculation\n')
-        calculationChoicePrompt = readline.question('');
-    }  
-*/
-/*
-    let firstNumberInput = readline.question('Enter your first number\n')
-    while(isNaN(firstNumberInput)) {
-        firstNumberInput = readline.question('This is not a number, please input the correct data Thank you\n')
-    }
-
-let secondNumberInput = readline.question('Enter your second number\n')
-    while(isNaN(secondNumberInput)) {
-        secondNumberInput = readline.question('This is not a number, please input the correct data\n')
-    }
-
-// let finalCalculation = 
-
-// let initialNumber = num1
-/*
-let gatePassQuestion = readline.question('To pass through the gate, you must answer this question: What is the product of 3 and 4?\n'); {
-    while (gatePassQuestion !== numberThree.toString() && mathTry < maxTries) { // Used a while loop to validate the input
-        console.log('Incorrect. Please try again.');
-        gatePassQuestion = readline.question(`What is the product of ${numberOne} and ${numberTwo}?\n`); // Asked the user to answer the question again
-        mathTry++; // Incremented the attempt counter
-    };
-    if (gatePassQuestion === numberThree.toString()) { //converting the input to a string to accept the user input
-        console.log('Correct! You may pass through the gate.\n\nYou look up and read a sign that says: \n\"To slay the dragon, you must go left, bring your COURAGE as it will be the KEY to your success!\n\"To rescue the princess, you must go right, bring your wits and make haste as the KEY to freeing her will only come at high NOON!\"'); // Used a template literal to create a message
-    }
-}
-*/
